6211e6af1d2c5fb63a753ad42b1503f54d3e4a64
Cagri Eren <69125059+cagri26ren@users.noreply.github.com>
2022-02-23 06:28:44 +0300

    Test case 2 implemented

--- Selenium/tests.js
+++ Selenium/tests.js
@@ -1,6 +1,8 @@
-const {Builder, By, Key} = require("selenium-webdriver");
+const {Builder, By, Key, until} = require("selenium-webdriver");
 const assert = require("assert");
 const { lstat } = require("fs");
+const { Driver } = require("selenium-webdriver/chrome");
+const { Browser } = require("selenium-webdriver");
 
 runTestsOneByOne();
 
@@ -8,6 +10,7 @@ async function runTestsOneByOne()
 {
     await checkCountryCodes();
     await checkAllASCII();
+    await checkButtonDisableFacebookLogin();
 }
 
 //clicks each country code in the code list to check if they all return the expected values
@@ -88,6 +91,36 @@ async function checkAllASCII()
     catch (err) 
     {
         console.log("Test case #5 failed: The expected and actual user IDs do not match.");
+    }
+
+    await driver.quit();
+}
+
+//TEST CASE 2: The user should not be able to click the Facebook login button repeatedly
+async function checkButtonDisableFacebookLogin() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.manage().window().maximize();
+    await driver.get("https://www.netflix.com/tr/Login");
+    await driver.findElement(By.className("btn minimal-login btn-submit btn-small")).click();
+    await driver.sleep(5000);
+
+    let windows = await driver.getAllWindowHandles();
+    await driver.switchTo().window(windows[1]);
+    let loginButton = await driver.findElement(By.name("login"));
+    await loginButton.click();
+    loginButton = await driver.findElement(By.name("login"));
+    await loginButton.click();
+    loginButton = await driver.findElement(By.name("login"));
+    let value = await loginButton.isEnabled();
+
+    try
+    {
+        assert.strictEqual(value, true);
+        console.log("Test case #2 is successful.");
+    }
+    catch (err) 
+    {
+        console.log("Test case #2 failed: Login button is not disabled and clicked repeatedly.");
     }
 
     await driver.quit();
