c307aedabe944fa5d5c7416e7fb4ed3a1172be30
ignatyus <ignatyus@outlook.com>
2022-02-26 12:09:19 +0300

    added log-page

---
+++ netflix-login-page/login.js
@@ -0,0 +1,25 @@
+function validate(){
+            var e_mail= document.getElementById("email").value;
+            var passw = document.getElementById("password").value;
+            if(e_mail =="metehan"){
+                if( passw = "886"){
+                alert("Sign in successful!");
+                return true;
+                }
+            }
+            else 
+                alert("uncessful login lol");
+                 
+            
+        }
+       // else{
+            //alert("Sign in failed")
+           // return false;
+        //}
+   // }
+//}
+
+function myFunction(x){
+   
+}
+
