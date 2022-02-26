7678b52973865afb188e33bdbcbda4e99b763daa
Cagri Eren <69125059+cagri26ren@users.noreply.github.com>
2022-02-26 20:53:04 +0300

    Login error implemented

--- netflix-login-page/login.js
+++ netflix-login-page/login.js
@@ -1,25 +1,39 @@
+
 function validate(){
-            var e_mail= document.getElementById("email").value;
-            var passw = document.getElementById("password").value;
-            if(e_mail =="metehan"){
-                if( passw = "886"){
-                alert("Sign in successful!");
-                return true;
-                }
-            }
-            else 
-                alert("uncessful login lol");
-                 
-            
+    var e_mail= document.getElementById("email").value;
+    var passw = document.getElementById("password").value;
+    if(e_mail =="metehan"){
+        if( passw == "886"){
+            alert("Sign in successful!");
+            localStorage.setItem("loginError", "false");
+            return true;
         }
-       // else{
-            //alert("Sign in failed")
-           // return false;
-        //}
-   // }
-//}
+    }
+    else{
+        localStorage.setItem("loginError", "true");
+        return false;
+    }
+    return false;
+
+}
+
+
 
 function myFunction(x){
    
 }
 
+function showLoginError() {
+    if (localStorage.getItem("loginError") == "true") {
+        console.log("a");
+        let loginErrorDiv = document.getElementById("login-error-div");
+        let insertedElement = document.createElement('div');
+        insertedElement.className = "loginError";
+        insertedElement.innerHTML = 'Bu e‑posta adresi ile bağlantılı bir hesap bulamadık. Lütfen yeniden deneyin ya da yeni bir hesap oluşturun.';
+        loginErrorDiv.appendChild(insertedElement);
+    }
+}
+
+function onRefresh() {
+    localStorage.setItem("loginError", "false");
+}
